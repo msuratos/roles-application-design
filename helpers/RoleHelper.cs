56727c634cd875866c84a805d7c3be2e0d1db53b
Melvin Suratos <melvinsuratos@hotmail.com>
2020-12-19 21:07:12 -0800

    Updated RoleHelper for better access & added another method to check tabs for role

--- helpers/RoleHelper.cs
+++ helpers/RoleHelper.cs
@@ -4,10 +4,11 @@ using System.Linq;
 
 namespace Enumtest.helpers
 {
+    [Flags]
+    internal enum Tabs { Empty = 0, Tab1 = 1, Tab2 = 2, Tab3 = 4 };
+
     internal static class RoleEnumHelper
     {
-        [Flags]
-        private enum Tabs { Empty = 0, Tab1 = 1, Tab2 = 2, Tab3 = 4 };
         private enum RolesEnum { Role1, Role2, Role3 };
 
         private static IDictionary<Guid, RolesEnum> staticRoles = new Dictionary<Guid, RolesEnum>() {
@@ -19,6 +20,30 @@ namespace Enumtest.helpers
             return Enum.GetValues(typeof(Tabs)).Cast<Tabs>().Where(t => (tabs & t) != 0).Select(r => r.ToString());
         }
 
+        private static Tabs GetTabsForRole(Guid role) {
+            Tabs tabsEnum = Tabs.Empty;
+
+            switch (staticRoles.Single(s => s.Key == role).Value)
+            {
+                case RolesEnum.Role1:
+                    tabsEnum |= Tabs.Tab1 | Tabs.Tab2;
+                    break;
+                case RolesEnum.Role2:
+                    tabsEnum |= Tabs.Tab1 | Tabs.Tab3;
+                    break;
+            }
+
+            return tabsEnum;
+        }
+
+        internal static bool CanRoleAccessTab(Guid role, Tabs tabs) {
+            //TODO: Double check this conditional logic
+            if (GetTabsForRole(role) != tabs)
+                return false;
+
+            return true;
+        }
+
         internal static ICollection<string> GetValidTabsForRoles(Guid[] roles)
         {
             ICollection<string> tabsList = new List<string>();
@@ -28,15 +53,7 @@ namespace Enumtest.helpers
             {
                 if (staticRoles.Any(a => a.Key == role))
                 {
-                    switch (staticRoles.Single(s => s.Key == role).Value)
-                    {
-                        case RolesEnum.Role1:
-                            tabsEnum |= Tabs.Tab1 | Tabs.Tab2;
-                            break;
-                        case RolesEnum.Role2:
-                            tabsEnum |= Tabs.Tab1 | Tabs.Tab3;
-                            break;
-                    }
+                    tabsEnum = GetTabsForRole(role);
                 }
             }
 
